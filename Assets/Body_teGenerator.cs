8adbd529a33b24800b70e5df68857d06a111cb00
kouki <kouworld0123@gmail.com>
2017-11-16 14:12:15 +0900

    ハンドのリファクタリング

--- Assets/Body_teGenerator.cs
+++ Assets/Body_teGenerator.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class Body_teGenerator : MonoBehaviour {
 
     public GameObject body_tePrefab;
+	int width = Screen.width;
+	int height = Screen.height;
+
 	// Use this for initialization
 	void Start () {
-        InvokeRepeating("GenTe", 3, 1);
-	}
-	
+        InvokeRepeating("GenTe", 1, 2);
+		InvokeRepeating("GenTeR", 1, 2);
+	}	
 	// Update is called once per frame
 	void GenTe () {
-        
-        Instantiate(body_tePrefab, new Vector3(-2.5f + 5 * Random.value, 6, 0), Quaternion.identity);
+		Vector3 handPosition = Camera.main.ViewportToWorldPoint(new Vector3(Random.value, 1, Camera.main.nearClipPlane));
+		handPosition.z = 0;
+		GameObject te = Instantiate(body_tePrefab, handPosition, Quaternion.identity);
+		Body_teController updown = te.GetComponent<Body_teController> ();
+		updown.direction = 1;
     }
+
+	void GenTeR () {
+		Vector3 handPosition = Camera.main.ViewportToWorldPoint(new Vector3(Random.value, 0, Camera.main.nearClipPlane));
+		handPosition.z = 0;
+		GameObject te = Instantiate(body_tePrefab, handPosition, Quaternion.identity);
+		Body_teController downup = te.GetComponent<Body_teController> ();
+		downup.direction = -1;
+	}
 }
